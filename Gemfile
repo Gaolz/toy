be6e9aee3800081ffa522f33838fd3b335c00e0c
glz <glzztt@gmail.com>
2016-07-16 12:47:26 +0800

    Finish layout and routes

--- Gemfile
+++ Gemfile
@@ -10,7 +10,7 @@ gem 'coffee-rails', '4.2.1'
 gem 'jquery-rails', '4.1.1'
 gem 'turbolinks', '5.0.0'
 gem 'jbuilder', '2.4.1'
-
+gem 'bootstrap-sass', '3.3.6'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Rails.application.routes.draw do
-  get 'foo/bar'
 
-  get 'foo/baz'
-
-  get 'static_pages/home'
-  get 'static_pages/about'
-  get 'static_pages/help'
-  get 'static_pages/contact'
+  get '/home',    to: 'static_pages#home'
+  get '/about',   to: 'static_pages#about'
+  get '/help',    to: 'static_pages#help'
+  get '/contact', to: 'static_pages#contact'
+  get '/signup',  to: 'users#new'
 
   resources :microposts
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "users#index"
+  root "static_pages#home"
 end
