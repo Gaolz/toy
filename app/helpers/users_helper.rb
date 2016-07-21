c84d7b6d5460cdb360e09e8dacdf9ef13d3818c6
glz <glzztt@gmail.com>
2016-07-21 22:43:36 +0800

    Finish user signup

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+
+  # 返回指定用户的 gravatar
+  def gravatar_for(user, options={size: 80})
+    return false if user.nil?
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
