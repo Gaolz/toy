e6474c4041cea7004caf662802d5b189a1891eea
glz <glzztt@gmail.com>
2016-07-26 22:36:13 +0800

    Implement basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
 
-  get '/home',    to: 'static_pages#home'
-  get '/about',   to: 'static_pages#about'
-  get '/help',    to: 'static_pages#help'
-  get '/contact', to: 'static_pages#contact'
-  get '/signup',  to: 'users#new'
+  get     '/home',    to: 'static_pages#home'
+  get     '/about',   to: 'static_pages#about'
+  get     '/help',    to: 'static_pages#help'
+  get     '/contact', to: 'static_pages#contact'
+  get     '/signup',  to: 'users#new'
+  get     '/login',   to: 'sessions#new'
+  post    '/login',  to: 'sessions#create'
+  delete  '/logout', to: 'sessions#destroy'
 
   resources :microposts
   resources :users
