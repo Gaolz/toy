5e4a571d8e9a8ac1fba38e9908aca91f82762c00
glz <glzztt@gmail.com>
2016-08-07 21:13:05 +0800

    Fix production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   host = "still-refuge-62767.herokuapp.com"
   config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smtp_settins = {
+  ActionMailer::Base.smtp_settings = {
     address: "smtp.sendgrid.net",
     port: "587",
     authentication: :plain,
